3e2263464bd76b45e4f4b15986819236371eed9f
Marcus Blanchard <wotevenislif3@gmail.com>
2023-11-22 14:33:25 +0800

    added map gem

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,4 +2,6 @@ class Gym < ApplicationRecord
   has_many :memberships
   has_many :reviews
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_024911) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_054135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_024911) do
     t.integer "average_rating"
     t.string "name"
     t.string "preview"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_gyms_on_user_id"
   end
 
