702dd792ee2c98912006445d7ae85c951db4bfdb
bp <brunnopg@me.com>
2023-11-22 17:09:12 +0800

    done

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -3,6 +3,5 @@ class Gym < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   belongs_to :user
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+
 end
