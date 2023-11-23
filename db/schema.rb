528a65172a1d538d27c80e6ed84dcfb97a190620
Daniel Gonzalo <danicogonzalo@gmail.com>
2023-11-23 14:49:29 +0800

    rating migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_042548) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_063746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_042548) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "rating"
+    t.integer "rating"
     t.string "content"
     t.bigint "user_id", null: false
     t.bigint "gym_id", null: false
